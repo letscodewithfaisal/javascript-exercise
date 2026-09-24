19d1c9eaa9e4c9c0fa9dd1b26cb1a763c8d6eb71
letscodewithfaisal <cvraman902@gmail.com>
2026-09-24 17:33:33 +0530

    day02 :basics of fucntion

---
+++ day02.js
@@ -0,0 +1,68 @@
+// basics of funtion **
+/* age = 65;
+console.log(age);
+
+let age = 45;
+console.log(age);
+
+ age = 25;
+console.log(age);
+*/
+
+/*
+const age = 25;
+let name = 'piyush'
+
+console.log(age);
+console.log(name); */
+
+
+// what is function **?
+// function is a code of block which is set of resuable code
+// A block of code design to perform specific Task
+
+/* function sayHello(name) {
+    console.log('Hello ' + name);
+}
+ 
+sayHello('Faisal Siddiqui'); */
+
+
+// example-2 **
+// function add(a, b) {
+//   let sum = a + b;
+//   return sum;
+// }
+
+// let result = add(10, 20);
+
+// console.log(result);
+
+ // example-3 **
+//arrow function
+//        fn name        param      return
+// const isAllowedToVote = (age) => age >= 18;
+
+// const isUserAllowedYoOpenBankAcount = (age, minBalance) =>
+//   age >= 18 && minBalance >= 5000;
+
+// console.log(isAllowedToVote(23));
+
+// DATA STRUCTURE **
+// -memory mai data ko ek particular structure mei store karna 
+// subjectify naa kare to kitna badhiya hoga
+
+// let numOne = 5;
+// let numOne = 9;
+// console.log(numOne);
+
+
+
+// const numOne;
+// const numOne = 9;
+// console.log(numOne);
+
+let name = 'Faisal';
+let course = 'JavaScript';
+
+console.log(name);
