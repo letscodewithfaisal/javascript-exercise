92d48ac3bdcd2bddaf59cf2488023c2fb2d37b8f
letscodewithfaisal <cvraman902@gmail.com>
2026-09-24 17:25:57 +0530

    Day01 : js basics

---
+++ day01.js
@@ -0,0 +1,10 @@
+console.log('value of age is', age);
+var age = 45;
+console.log('adding 5 to 10', addFive(10));;
+console.log('value of age is', age);
+var age = 45;
+
+function addFive(number) {
+  var result = number + 5;
+  return result;
+}
