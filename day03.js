8a4609f9612df913e6365f3ed19f20ba916269e1
letscodewithfaisal <cvraman902@gmail.com>
2026-09-25 16:12:08 +0530

    day03: string,condion, odd even

---
+++ day03.js
@@ -0,0 +1,106 @@
+// **Store your Basic Information** //
+
+// let name = 'faisal';
+// let age = 23;
+// let city = 'Dwarka';
+
+// console.log(`My name is ${name}`);
+// console.log(`I am ${age} years old`);
+// console.log(`I Live in ${city}`);
+
+
+// let name = 'faisal';
+// let age = 23;
+// let city = 'Dwarka';
+
+// console.log("My name is" + name);
+// console.log("I am "  + age +  " years old");
+// console.log("I Live in " + city);
+
+// **Use Typeof** //
+// let name = 'Faisal';
+// let age = 23;
+// let isStudent = true;
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isStudent);
+
+// **Calculate Two Number** //
+
+// let a = 20;
+// let b = 10;
+ 
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+// **String ** //
+
+// let firstName = "Faisal";
+// let lastName = "Siddiqui";
+
+// console.log(firstName +" "+ lastName );
+
+// let name = "Faisal";
+// let age = 23;
+
+// console.log("My Name is " + name + " and " + " I am " + age + " year old.");
+// console.log(`My Name is ${name} and I am ${age} year old.`);
+
+// **Condition** //
+// **Check age
+
+// let age = 10;
+// if (age >= 18) {
+//   console.log("You can vote .");
+// }
+// else {
+//    console.log(" You can Not Vote .");
+// }
+ 
+// **Positive, negative, or zero
+
+// let number = -5;
+// if (number > 0){
+//   console.log("positive");
+// }
+
+// else if (number < 0) {
+//   console.log("Negative");
+// }
+
+// else   {
+//   console.log("Zero");
+// }
+
+//**  Even or Odd
+
+// let number = 7;
+
+// if (number % 2 === 0){
+//   console.log("Even");
+// }
+
+// else { 
+//   console.log("Odd");
+// }
+
+
+// **Loops** //
+// ** Print 1 to 10
+
+// for (let i = 1; i<=10; i++) {
+//   console.log(i);
+// }
+
+//** Print even numbers 
+let i = 1;     
+for (i = 1; i <= 20; i++ ) {
+       if (i%2 ===0) {
+    console.log(i);
+   
+  }}
+  
